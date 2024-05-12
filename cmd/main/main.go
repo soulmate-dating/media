9dd6cd60b9f5cd9d9975d20c5b377b1a934c3315
TobbyMax <ageev.maxim2003@gmail.com>
2024-05-12 15:16:15 +0300

    feat: TIN-103 refactor main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,54 +2,21 @@ package main
 
 import (
 	"context"
+	"github.com/soulmate-dating/media/internal/ports/grpc"
 	"log"
-	"net"
-	"os"
-	"time"
 
-	"golang.org/x/sync/errgroup"
-	"google.golang.org/grpc"
-
-	"github.com/soulmate-dating/media/internal/adapters/s3"
 	"github.com/soulmate-dating/media/internal/app"
-	"github.com/soulmate-dating/media/internal/graceful"
-	grpcSvc "github.com/soulmate-dating/media/internal/ports/grpc"
-)
-
-const (
-	grpcPort = ":8082"
+	"github.com/soulmate-dating/media/internal/config"
 )
 
 func main() {
 	ctx := context.Background()
 
-	s3Client, err := s3.NewClient("minio:9000", "accesskey", "secretkey", "media", time.Hour, false)
+	cfg, err := config.Load()
 	if err != nil {
-		log.Fatal(err.Error())
-	}
-	appSvc := app.NewApp(s3Client)
-
-	lis, err := net.Listen("tcp", grpcPort)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("failed to load config: %v", err)
 	}
 
-	svc := grpcSvc.NewService(appSvc)
-	grpcServer := grpc.NewServer(grpc.ChainUnaryInterceptor(
-		grpcSvc.UnaryLoggerInterceptor,
-		grpcSvc.UnaryRecoveryInterceptor(),
-	))
-	grpcSvc.RegisterMediaServiceServer(grpcServer, svc)
-
-	eg, ctx := errgroup.WithContext(ctx)
-
-	sigQuit := make(chan os.Signal, 1)
-	eg.Go(graceful.CaptureSignal(ctx, sigQuit))
-	// run grpc server
-	eg.Go(grpcSvc.RunGRPCServerGracefully(ctx, lis, grpcServer))
-
-	if err := eg.Wait(); err != nil {
-		log.Printf("gracefully shutting down the servers: %s\n", err.Error())
-	}
-	log.Println("servers were successfully shutdown")
+	appSvc := app.New(ctx, cfg)
+	grpc.Run(ctx, cfg, appSvc)
 }
